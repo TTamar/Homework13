5d94b58ab465bb8c19ac2973fcfef80fd1ffbaaa
Tamar Putkaradze <99253555+TTamar@users.noreply.github.com>
2022-04-04 16:16:59 +0400

    add js file

---
+++ app.js
@@ -0,0 +1,27 @@
+let UserNumber = 10;
+let UserName = "Jasper";
+let getRegister = true;
+let isTeacher = false;
+let Underfined;
+let UserNull = null;
+let UserNaN = NaN;
+
+// console.log(typeof UserNumber, typeof UserName, typeof getRegister, typeof isTeacher, typeof Underfined, typeof UserNull, typeof UserNaN);
+
+let myName = "Tamari";
+let myHobby = "Physics";
+let yearOfBirth = 1999;
+let currentyear = 2022;
+let myGoal = `my name is ${myName}, I am ${
+  currentyear - yearOfBirth
+} old and my hobby is ${myHobby} `;
+
+// console.log(myGoal);
+
+let myAge = currentyear-yearOfBirth;
+let myText = "my name is";
+let myText2 = "I am";
+let myText3 = "old and my hobby is";
+
+/*console.log(myText + " " + myName +", "+ myText2 + " " + myAge + " " + myText3 + " " + myHobby);
+console.log("I am vano" + " " + myName +", I am " + myAge + " old and my hobby is " + myHobby);*/
